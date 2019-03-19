d3a6464861ad654936f2155e291f32bb5bbc71b0
dlcjfgmlnasa <dlcjfjgmlnasa@naver.com>
2019-03-20 00:18:52 +0900

    create a block part two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previodHash: string;
     public date: string;
     public timestamp: number;
+
+    static calcuateBlockHash = (
+        index: number, 
+        previodHash: string,
+        timestamp: number,
+        date: string
+    ): string => CryptoJS.SHA256(index + previodHash + timestamp + date).toString()
 
     constructor(index: number, hash: string, previodHash: string, date: string, timestamp: number){
         this.index = index;
@@ -16,6 +25,12 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "202020202020", "", "Hello", 123456);
 
-let blockChain : [Block] = [genesisBlock];
-console.log(blockChain);
+let blockChain : Block[] = [genesisBlock];
+
+const getblockChain = ():Block[] => blockChain
+
+const getLastestBlock = ():Block => blockChain[blockChain.length - 1]
+
+const getNewTimeStamp = ():number => Math.round(new Date().getTime() / 1000)
+
 export {};
